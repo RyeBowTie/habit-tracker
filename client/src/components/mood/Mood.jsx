07454a54559a44da20763f1e614a0f66b4c7b569
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-31 17:37:30 -0400

    form

--- client/src/components/mood/Mood.jsx
+++ client/src/components/mood/Mood.jsx
@@ -5,7 +5,6 @@ import { useState }  from 'react';
 
 
 
-
 export default function Mood() {
     const [buttonPopup, setButtonPopup] = useState(false);
 
@@ -13,24 +12,25 @@ export default function Mood() {
         <div className="mood" id="mood"> 
              <h1>Mood</h1>
                <div className ="container">
+            
+             {/* HAPPY SECTION */}
                    <div className="item">
                     <img  onClick={() => setButtonPopup(true)} id="smile"
                     src="./assets/smile.png" alt=""
                     />
                     <Popup trigger ={buttonPopup} setTrigger ={setButtonPopup}>
-
+                   
                     </Popup>
                     </div>
+                        {/* SAD SECTION */}
                     <div className="item" >
                    <img onClick={() => setButtonPopup(true)} id="sad"
                     src="./assets/sad.png" alt=""
                     />
                     <Popup trigger={buttonPopup} setTrigger= {setButtonPopup}>
-                    <form>
-                   
-            </form>
                     </Popup>
                     </div>
+                        {/* ANGRY SECTION */}
                     <div className="item">
                     <img onClick={() => setButtonPopup(true)} id="angry"
                     src="./assets/angry.png" alt=""
@@ -39,12 +39,12 @@ export default function Mood() {
 
                     </Popup>
                    </div>
+                               {/* MIXED EMOTION SECTION */}
                    <div className="item">
                     <img onClick={() => setButtonPopup(true)} id="emotion"
                     src="./assets/emotions.png" alt=""/>
                      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                          </Popup>
-            
                     </div>
                       </div>
                </div>
